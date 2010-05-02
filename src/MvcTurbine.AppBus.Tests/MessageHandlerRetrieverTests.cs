fc0a4d407709e3f804578a766706499dfa9c324a
darrencauthon <darren@cauthon.com>
2010-05-02 15:03:18 -0500

    Add message handler retriever.

---
+++ src/MvcTurbine.AppBus.Tests/MessageHandlerRetrieverTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using AppBus;
+using AutoMoq;
+using NUnit.Framework;
+
+namespace MvcTurbine.AppBus.Tests
+{
+    [TestFixture]
+    public class MessageHandlerRetrieverTests
+    {
+        private AutoMoqer mocker;
+
+        [SetUp]
+        public void TestSetup()
+        {
+            mocker = new AutoMoqer();
+        }
+
+        [Test]
+        public void Should_return_all_message_handlers_in_the_current_domain()
+        {
+            var retriever = mocker.Resolve<MessageHandlerRetriever>();
+
+            var results = retriever.GetAllMessageHandlerTypes();
+
+            Assert.AreEqual(5, results.Count());
+            Assert.Contains(typeof (Test1), results.ToList());
+            Assert.Contains(typeof (Test2), results.ToList());
+        }
+
+        public class Test1 : IMessageHandler
+        {
+            public bool CanHandle(Type type)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Handle(object message)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public class Test2 : IMessageHandler
+        {
+            public bool CanHandle(Type type)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Handle(object message)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
--- src/MvcTurbine.AppBus/MessageHandlerRetriever.cs
+++ src/MvcTurbine.AppBus/MessageHandlerRetriever.cs
@@ -1,13 +1,37 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using AppBus;
 
 namespace MvcTurbine.AppBus
 {
+    public interface IMessageHandlerRetriever
+    {
+        IEnumerable<Type> GetAllMessageHandlerTypes();
+    }
+
     public class MessageHandlerRetriever : IMessageHandlerRetriever
     {
         public IEnumerable<Type> GetAllMessageHandlerTypes()
         {
-            throw new NotImplementedException();
+            var list = new List<Type>();
+
+            foreach (var assembly in GetAllAssemblies())
+                list.AddRange(GetAllMessageHandlersInThisAssembly(assembly));
+
+            return list;
+        }
+
+        private static IEnumerable<Type> GetAllMessageHandlersInThisAssembly(Assembly assembly)
+        {
+            return assembly.GetTypes()
+                .Where(x => x.GetInterfaces().Contains(typeof (IMessageHandler)));
+        }
+
+        private static IEnumerable<Assembly> GetAllAssemblies()
+        {
+            return AppDomain.CurrentDomain.GetAssemblies().ToList();
         }
     }
 }
